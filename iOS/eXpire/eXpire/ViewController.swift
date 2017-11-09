dc1ac7fa65ff9e6c2882f98e7bc9002928e58171
Scott English <scott.h.english@gmail.com>
2017-11-09 16:41:41 -0600

    Establishing UI variables and save method.

--- iOS/eXpire/eXpire/ViewController.swift
+++ iOS/eXpire/eXpire/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fooditemTextField: UITextField!
+    @IBOutlet weak var foodtypeTextField: UITextField!
+    @IBOutlet weak var foodquantityTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func SaveFood(_ sender: UIButton) {
+        
+    }
+    
 }
 
