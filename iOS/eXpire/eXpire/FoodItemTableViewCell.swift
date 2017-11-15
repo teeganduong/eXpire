9b19a0b8189639455ab8546c5ee1476e5c66c65a
Scott English <scott.h.english@gmail.com>
2017-11-15 17:30:49 -0600

    Added home screen table view

---
+++ iOS/eXpire/eXpire/FoodItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodItemTableViewCell.swift
+//  eXpire
+//
+//  Created by Scott English on 11/15/17.
+//  Copyright © 2017 Scott English. All rights reserved.
+//
+
+import UIKit
+
+class FoodItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
